b57e4329b19e7d83b4cc047b2daf94e0baea9f7a
Saunak Ghosh <saunakghosh710@gmail.com>
2023-10-07 02:53:45 +0530

    first commit

---
+++ src/config/stripe.ts
@@ -0,0 +1,30 @@
+// price_1Nxpr8SE1wyod7qCGXGu8s8Y
+
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: 10,
+    pagesPerPdf: 5,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 50,
+    pagesPerPdf: 25,
+    price: {
+      amount: 20,
+      priceIds: {
+        test: "price_1Nxpr8SE1wyod7qCGXGu8s8Y",
+        production: "",
+      },
+    },
+  },
+];
